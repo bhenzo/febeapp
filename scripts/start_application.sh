697cec8e692a5f27a3ebe1404940870cc58a9a4a
Enzo <scalone.enzo@gmail.com>
2023-07-16 20:06:57 -0300

    test

---
+++ scripts/start_application.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+screen -dmS escalenzo-link cd /home/ubuntu/apps/target && java -jar febe.jar
---
+++ scripts/stop_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/scripts
+
+echo 'pruebalog from repo' >> log.txt
+echo 'Application stop script'
