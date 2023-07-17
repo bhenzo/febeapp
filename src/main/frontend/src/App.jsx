428d455073576d563b1b1413648dd30fd07b9e8a
Enzo <scalone.enzo@gmail.com>
2023-07-17 12:23:59 -0300

    test

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,39 +1,26 @@
-import {
-    Box,
-    Card,
-    CardContent,
-    Container,
-    FormGroup,
-    Input,
-    ThemeProvider,
-    createTheme,
-} from "@mui/material";
-import "./App.css";
-import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Card, CardContent, Container, FormGroup, Input, ThemeProvider, createTheme } from '@mui/material';
+import './App.css';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
-    return (
-        <Container>
-            <Box>
-                <Card variant="outlined">
-                    <CardContent>
-                        <Box flexDirection="row">
-                            <Box>
-                                <Button variant="contained">
-                                    <DeleteIcon />
-                                    &nbsp;Delete
-                                </Button>
-                            </Box>
-                            <Box m={2}>
-                                <Input placeholder="Ingrese coso"></Input>
-                            </Box>
-                        </Box>
-                    </CardContent>
-                </Card>
-            </Box>
-        </Container>
-    );
+	return (
+		<Container>
+			<Box>
+				<Box flexDirection="row">
+					<Box>
+						<Button variant="contained">
+							<DeleteIcon />
+							&nbsp;Delete
+						</Button>
+					</Box>
+					<Box m={2}>
+						<Input placeholder="Ingrese coso"></Input>
+					</Box>
+				</Box>
+			</Box>
+		</Container>
+	);
 }
 
 export default App;
