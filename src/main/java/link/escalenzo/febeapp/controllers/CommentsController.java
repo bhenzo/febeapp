68b209a2906ee92fe5a611441a4d6aac422bf188
Enzo <scalone.enzo@gmail.com>
2023-07-16 22:13:49 -0300

    test env vars

--- src/main/java/link/escalenzo/febeapp/controllers/CommentsController.java
+++ src/main/java/link/escalenzo/febeapp/controllers/CommentsController.java
@@ -2,6 +2,7 @@ package link.escalenzo.febeapp.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,17 @@ import link.escalenzo.febeapp.dto.Comment;
 @RestController
 public class CommentsController {
 
+	@Value("user")
+	private String user;
+	@Value("password")
+	private String pass;
+	
 	
 	@GetMapping("/comments")
 	public List<Comment> getComments(){
 		return List.of(
-				new Comment(2L, "Comentario 1"),
-				new Comment(3L, "Comentario 2")
+				new Comment(2L, "Comentario 1 " + user),
+				new Comment(3L, "Comentario 2 " + pass)
 				);
 	}
 }
