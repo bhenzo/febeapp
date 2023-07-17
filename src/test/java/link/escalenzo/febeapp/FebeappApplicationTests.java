9534ecd4134e6ddaa4dad2ce786f1bbe6c33cc5b
Enzo <scalone.enzo@gmail.com>
2023-07-16 21:35:07 -0300

    failed test check

--- src/test/java/link/escalenzo/febeapp/FebeappApplicationTests.java
+++ src/test/java/link/escalenzo/febeapp/FebeappApplicationTests.java
@@ -2,12 +2,14 @@ package link.escalenzo.febeapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class FebeappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(false);
 	}
 
 }
